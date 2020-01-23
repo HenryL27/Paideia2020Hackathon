3077bd556c52ecdc4e005df2dc2ede2cb944d13e
Ian Wahbe <ian@wahbe.com>
2020-01-22 17:34:00 -0800

    pretty print

--- solv.py
+++ solv.py
@@ -1,18 +1,47 @@
+import random
+
+
 def solve_sudoku(lis):
-    if not 0 in lis: # no empty spaces
+    if not 0 in lis:  # no empty spaces
         return lis
-    n = lis.index(0) # first empty space
-    for i in range(1,10): # ranging throught possible numbers
-        fail = False # i does not work for n
-        for k in range(81): # over each element in the array
+    n = lis.index(0)  # first empty space
+    numbers_to_range_over = list(range(1, 10))
+    random.shuffle(numbers_to_range_over)
+    for i in numbers_to_range_over:  # ranging throught possible numbers
+        fail = False  # i does not work for n
+        for k in range(81):  # over each element in the array
             if lis[k] == i:
                 # test for a conflict in row and column and square
-                if k % 9 == n % 9 or k // 9 == n // 9 or (k // 27 == n // 27 and (k // 3) % 3 == (n // 3) % 3):
+                if k % 9 == n % 9 or k // 9 == n // 9 or (
+                        k // 27 == n // 27 and (k // 3) % 3 == (n // 3) % 3):
                     fail = True
                     break
         if fail:
             continue
-        x = solve_sudoku(lis[:n] + [i] + lis[n+1:]) # solve on the updated list
+        x = solve_sudoku(lis[:n] + [i] +
+                         lis[n + 1:])  # solve on the updated list
         if not x == False:
             return x
     return False
+
+
+def generate_sudoku():
+    empty_board = [0] * 81
+    return solve_sudoku(empty_board)
+
+
+def display_sudoku(sudoku):
+    print("+" + "-" * 20 + "+")
+    for i in range(0, 9):
+        print("|", end="")
+        for j in range(0, 9):
+            print(str(sudoku[i * 9 + j]) + " ", end="")
+            if (j + 1) % 3 == 0:
+                print("|", end="")
+        if (i + 1) % 3 == 0 and i != 8:
+            print("\n|" + "-" * 20 + "|", end="")
+        print("", end="\n")
+    print("+" + "-" * 20 + "+", end="\n")
+
+
+display_sudoku(generate_sudoku())
--- sudoko_functions.cpp
+++ sudoko_functions.cpp
@@ -1,3 +1,18 @@
+#include <vector>
 
+int sudoku_board_access(const std::vector<int> &sudoku, int row, int column)
+{
+  return sudoku.at(9 * (row - 1) + column);
+}
 
-std::vector
+std::vector<int> find_possible_options(const std::vector<int> sudoku, int row,
+                                       int column)
+{
+  std::vector<int> out{1, 2, 3, 4, 5, 6, 7, 8, 9};
+  for (int i = 1; i <= 9; i++) {
+    if
+      out.sudoku_board_access(sudoku, column, i)
+  }
+}
+
+int main() { return 0; }
